6de85ef0007bbb088a00ecc3eec15ef020eab629
Alex Cruden <alexscruden@gmail.com>
2023-11-28 14:21:28 +0000

    index, show and new methods created

--- app/controllers/fixtures_controller.rb
+++ app/controllers/fixtures_controller.rb
@@ -1,2 +1,34 @@
 class FixturesController < ApplicationController
+  def index
+    @league = League.find(params[:league_id])
+    @fixtures = @league.fixtures.all
+  end
+
+  def show
+    @fixture = Fixture.find(params[:id])
+  end
+
+  def new
+    @fixture = Fixture.new
+  end
+
+  def create
+    @
+  end
+
+  def edit
+
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def fixture_params
+    params.require(:fixture).permit()
+  end
 end
