1f32457da45d68bb9115e6b99b47c1bb528484d3
Zeinab Warsama <swarsama@hotmail.co.uk>
2023-11-28 11:19:32 +0000

    add league model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_27_164309) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_28_111744) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "leagues", force: :cascade do |t|
+    t.string "name"
+    t.string "format"
+    t.integer "start_date"
+    t.string "level"
+    t.string "league_type"
+    t.integer "number_of_teams"
+    t.integer "days_per_week"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
