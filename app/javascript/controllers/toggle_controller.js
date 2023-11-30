86936c8067518169f4ef6eb1cf8f17288015a73a
Alex Cruden <alexscruden@gmail.com>
2023-11-30 17:14:42 +0000

    tiny final tweak on toggle button

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -9,6 +9,8 @@ export default class extends Controller {
 
   fire() {
     this.togglableElementTarget.classList.toggle("d-none");
+    this.toggleButtonTarget.classList.toggle("btn-primary");
+    this.toggleButtonTarget.classList.toggle("btn-secondary");
 
   }
 }
