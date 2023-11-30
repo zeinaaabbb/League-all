158b7a52137673d4ee2ba2ba517be5a5479914a6
Alex Cruden <alexscruden@gmail.com>
2023-11-30 16:48:33 +0000

    form for team registration now toggles

---
+++ app/javascript/controllers/toggle_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["togglableElement", "toggleButton"]
+
+  connect() {
+    console.log("Hello from toggle_controller.js")
+  }
+
+  fire() {
+    this.togglableElementTarget.classList.toggle("d-none");
+    this.toggleButtonTarget.classList.toggle("d-none");
+
+  }
+}
