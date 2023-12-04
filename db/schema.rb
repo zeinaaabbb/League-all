3eae258e6f2c24ba482992e6ff05a77a054e756c
Alex Cruden <alexscruden@gmail.com>
2023-12-04 15:25:01 +0000

    created migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.1].define(version: 2023_12_04_114239) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_04_152146) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -142,6 +141,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_04_114239) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "location"
     t.index ["user_id"], name: "index_teams_on_user_id"
   end
 
