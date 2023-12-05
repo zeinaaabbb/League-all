734506c10a8020962285d5c65df40a39da05395a
Alex Cruden <alexscruden@gmail.com>
2023-12-05 17:00:42 +0000

    fixtures still had gameweek 9 top, this has been changed back to 1

--- app/controllers/league_teams_join_controller.rb
+++ app/controllers/league_teams_join_controller.rb
@@ -1,17 +1,18 @@
 class LeagueTeamsJoinController < ApplicationController
   def create
-    @league_team = LeagueTeamsJoin.new(league_teams_join_params)
     @league = League.find(params[:league_id])
     @team = Team.find(params[:league_teams_join][:team_id])
-    @league_team.team = @team
-    @league_team.league = @league
-    if LeagueTeamsJoin.where(team_id: @team.id, league_id: @league.id) == nil
+    if LeagueTeamsJoin.find_by(team_id: @team.id, league_id: @league.id) == nil
+      @league_team = LeagueTeamsJoin.new(league_teams_join_params)
+      @league_team.team = @team
+      @league_team.league = @league
       if @league_team.save!
         redirect_to league_path(@league), notice: "Your request has been submitted. The owner of #{@league.name} will respond soon."
       else
         render :new, status: :unprocessable_entity
       end
     else
+      raise
       redirect_to league_path(@league), notice: "You have already sent a request for #{@league.name}. The league owner will respond soon."
     end
   end
