b769ddcce1a82afafe73d161e61d8a04c2cd2d1b
Zeinab Warsama <swarsama@hotmail.co.uk>
2023-12-07 13:05:15 +0000

    Updated on the messags

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -8,7 +8,7 @@ export default class extends Controller {
   // array that defines the targets
   static targets = ["messages"]
   // values that the controller recieves
-  static values = { chatroomId: Number }
+  static values = { chatroomId: Number, currentUserId: Number }
 
   connect() {
     // console.log("im connected!")
@@ -28,6 +28,41 @@ export default class extends Controller {
         }
       }
     )
+  }
+
+  #buildMessageElement(currentUserIsSender, message) {
+    return `
+      <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+        <div class="${this.#userStyleClass(currentUserIsSender)}">
+          ${message}
+        </div>
+      </div>
+    `
+  }
+  #justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
+  }
+
+  #userStyleClass(currentUserIsSender) {
+    return currentUserIsSender ? "sender-style" : "receiver-style"
+  }
+
+  #insertMessageAndScrollDown(data) {
+    // Logic to know if the sender is the current_user
+    const currentUserIsSender = this.currentUserIdValue === data.sender_id
+
+    // Creating the whole message from the `data.message` String
+    const messageElement = this.#buildMessageElement(currentUserIsSender, data.message)
+
+    // Inserting the `message` in the DOM
+    this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  }
+
+  #insertMessageAndScrollDown(data) {
+    // Logic to know if the sender is the current_user
+    const currentUserIsSender = this.currentUserIdValue === data.sender_id
+    // [...]
   }
 
   #insertMessageAndScrollDown(data) {
